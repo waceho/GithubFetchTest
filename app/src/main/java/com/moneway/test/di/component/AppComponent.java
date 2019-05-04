248aae6623a3431e04af3177283686a21ddbf844
waceho <amancio.perros@gmail.com>
2019-05-04 18:53:31 +0200

    complete dagger component

---
+++ app/src/main/java/com/moneway/test/di/component/AppComponent.java
@@ -0,0 +1,28 @@
+package com.moneway.test.di.component;
+
+import android.app.Application;
+
+import com.moneway.test.MonewayTestApp;
+import com.moneway.test.di.builder.ActivityBuilder;
+
+import javax.inject.Singleton;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjectionModule;
+
+@Singleton
+@Component(modules = {AndroidInjectionModule.class, ActivityBuilder.class})
+public interface AppComponent {
+    // inject App
+    void inject(MonewayTestApp monewayTestApp);
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}
