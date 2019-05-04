ee6a649a3fa3ce8120a99abb47b6d2031f1a6d5b
waceho <amancio.perros@gmail.com>
2019-05-04 18:40:32 +0200

    create Application component

---
+++ app/src/main/java/com/moneway/test/MonewayTestApp.java
@@ -0,0 +1,34 @@
+package com.moneway.test;
+
+import android.app.Activity;
+import android.app.Application;
+
+import com.moneway.test.di.component.DaggerAppComponent;
+
+import javax.inject.Inject;
+
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+
+public class MonewayTestApp extends Application implements HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> activityDispatchingAndroidInjector;
+
+    @Override
+    public DispatchingAndroidInjector<Activity> activityInjector() {
+        return activityDispatchingAndroidInjector;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // build dagger
+        DaggerAppComponent.builder()
+                .application(this)
+                .build()
+                .inject(this);
+
+    }
+}
